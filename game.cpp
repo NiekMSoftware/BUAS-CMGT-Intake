79802af37d4c80de6a3542413621bff6f0ba2c7a
Niek Melet <niek+git@meletstudios.com>
2024-12-13 15:26:38 +0100

    New Commit

---
+++ game.cpp
@@ -0,0 +1,23 @@
+// Template, 2024 IGAD Edition
+// Get the latest version from: https://github.com/jbikker/tmpl8
+// IGAD/NHTV/BUAS/UU - Jacco Bikker - 2006-2024
+
+#include "precomp.h"
+#include "game.h"
+
+// -----------------------------------------------------------
+// Initialize the application
+// -----------------------------------------------------------
+void Game::Init()
+{
+	// anything that happens only once at application start goes here
+}
+
+// -----------------------------------------------------------
+// Main application tick function - Executed once per frame
+// -----------------------------------------------------------
+void Game::Tick( float /* deltaTime */ )
+{
+	screen->Clear(0);
+	screen->Print("Hello, World!\n", 10, 10, 0xFFFFFFFF);
+}
